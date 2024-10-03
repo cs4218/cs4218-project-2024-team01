6a5e299ddfdf1cadbc8b3ffd305a5bee8570c737
Ong Wen Pin <wenpin2108@gmail.com>
2024-10-03 14:21:52 +0800

    Add test for user management in authcontroller

---
+++ testutils/user/userbuilder.js
@@ -0,0 +1,52 @@
+export class UserBuilder {
+	constructor() {
+		this.userObj = {
+			_id: "123123",
+			name: "Test USer 123",
+			email: 'test123@example.com',
+			phone: '90000123',
+			address: 'Test Address 345',
+			answer: 'Test answer',
+			password: 'password',
+			role: 0
+		}
+	}
+	withName(name) {
+		this.userObj.name = name
+		return this
+	}
+	
+	withEmail(email) {
+		this.userObj.email = email
+		return this
+	}
+	
+	withPhone(phone) {
+		this.userObj.phone = phone
+		return this
+	}
+	
+	withAddress(address) {
+		this.userObj.address = address
+		return this
+	}
+	
+	withAnswer(answer) {
+		this.userObj.answer = answer
+		return this
+	}
+	
+	withPassword(password) {
+		this.userObj.password = password
+		return this
+	}
+	
+	withRole(role) {
+		this.userObj.role = role
+		return this
+	}
+	
+	build() {
+		return this.userObj
+	}
+}
