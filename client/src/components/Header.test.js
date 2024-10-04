ba5ea0d6c8474562b6ac04249488ad5e3787c63d
Hau Chong <56034480+hauchongtang@users.noreply.github.com>
2024-10-05 00:34:33 +0800

    Add test cases for privileged routing

--- client/src/components/Header.test.js
+++ client/src/components/Header.test.js
@@ -6,6 +6,7 @@ import * as router from 'react-router';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import Header from './Header';
+import { useAuth } from '../context/auth';
 
 // Mocking axios.post
 jest.mock('axios');
@@ -14,7 +15,7 @@ jest.mock('react-hot-toast');
 jest.mock('../context/auth', () => ({
   useAuth: jest.fn(() => [{
     success: true,
-    user: { id: 1, name: 'John Doe', email: 'test@example.com' },
+    user: { id: 1, name: 'John Doe', email: 'test@example.com', role: 0 },
     token: 'mockToken'
   }, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
 }));
@@ -47,20 +48,19 @@ window.matchMedia = window.matchMedia || function () {
 const navigate = jest.fn();
 describe('Header component', () => {
   beforeEach(() => {
-    jest.mock('../context/auth', () => ({
-      useAuth: jest.fn(() => [{
-        success: true,
-        user: { id: 1, name: 'John Doe', email: 'test@example.com' },
-        token: 'mockToken'
-      }, jest.fn()]) // Mock useAuth hook to return null state and a mock function for setAuth
-    }));
-
     jest.clearAllMocks();
     jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+    // For useCategory hook
     axios.get.mockResolvedValueOnce({
-      data: null
+      data: {
+        category: [{
+          slug: 'test-category',
+          name: 'Test Category',
+        }]
+      }
     })
   });
+
   it('should logout successfully', async () => {
     let localStorageSpy = jest.spyOn(window.localStorage, 'removeItem');
     const { getByText } = render(
@@ -98,4 +98,46 @@ describe('Header component', () => {
     expect(toast.success).toHaveBeenCalledTimes(1);
     expect(toast.success).toHaveBeenCalledWith('Logout Successfully');
   });
+
+  const navigateReturnObj = {"preventScrollReset": undefined, "relative": undefined, "replace": false, "state": undefined, "unstable_viewTransition": undefined}
+  it('should navigate to user dashboard', async () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </MemoryRouter>);
+
+    const dashboardButton = getByText('Dashboard');
+    fireEvent.click(dashboardButton);
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1));
+    });
+
+    expect(navigate).toHaveBeenCalledTimes(1);
+    expect(navigate).toHaveBeenCalledWith("/dashboard/user", navigateReturnObj);
+  })
+
+  it('should navigate to admin dashboard', async () => {
+    useAuth.mockReturnValueOnce([{
+      success: true,
+      user: { id: 1, name: 'John Doe', email: 'test@example.com', role: 1 },
+      token: 'mockToken'
+    }])
+    const { getByText } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </MemoryRouter>);
+
+    const dashboardButton = getByText('Dashboard');
+    fireEvent.click(dashboardButton);
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1));
+    });
+
+    expect(navigate).toHaveBeenCalledTimes(1);
+    expect(navigate).toHaveBeenCalledWith("/dashboard/admin", navigateReturnObj);
+  })
 });
